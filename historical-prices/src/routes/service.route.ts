0a888a9d99e96b824f3ced9c0f333d3420c86d6f
Felipe Lemos <>
2024-07-30 17:21:09 +0200

    Feat: test route

--- historical-prices/src/routes/service.route.ts
+++ historical-prices/src/routes/service.route.ts
@@ -4,7 +4,7 @@ import { post } from '../controllers/service.controller';
 
 const serviceRouter = Router();
 
-serviceRouter.post('/', (req, res) => {
+serviceRouter.post('/historical-prices', (req, res) => {
   logger.info('Service post message received');
   logger.info('Request body: ' + JSON.stringify(req.body));
    res.status(200).end();
