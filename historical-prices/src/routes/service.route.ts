cb5bc8678693cf117b7e28bf58498de5caaf32a1
Felipe Lemos <>
2024-07-30 15:35:02 +0200

    Try: reeturn 200

--- historical-prices/src/routes/service.route.ts
+++ historical-prices/src/routes/service.route.ts
@@ -6,7 +6,8 @@ const serviceRouter = Router();
 
 serviceRouter.post('/', (req, res, next) => {
   logger.info('Service post message received');
-
+  logger.info('Request body: ' + JSON.stringify(req.body));
+  return 200;
   try {
     post(req, res);
   } catch (error) {
