c9a8bd17b0833c8dcb53de9ea901f58a0348c187
Felipe Lemos <>
2024-07-30 17:00:35 +0200

    Test

--- historical-prices/src/routes/service.route.ts
+++ historical-prices/src/routes/service.route.ts
@@ -4,15 +4,17 @@ import { post } from '../controllers/service.controller';
 
 const serviceRouter = Router();
 
-serviceRouter.post('/', (req, res, next) => {
+serviceRouter.post('/', (req, res) => {
   logger.info('Service post message received');
   logger.info('Request body: ' + JSON.stringify(req.body));
-  return 200;
+   res.status(200).end();
+   /**
   try {
     post(req, res);
   } catch (error) {
     next(error); 
   }
+     */
 });
 
 export default serviceRouter;
