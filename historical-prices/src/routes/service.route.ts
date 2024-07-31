8941399db8f59b94ff0da9b8dd9a8feea9b32e6f
Felipe Lemos <>
2024-07-31 11:32:07 +0200

    Remove res

--- historical-prices/src/routes/service.route.ts
+++ historical-prices/src/routes/service.route.ts
@@ -1,23 +1,14 @@
 import { Router } from 'express';
 import { logger } from '../utils/logger.utils';
 //import { post } from '../controllers/service.controller';
-import { apiSuccess } from '../api/success.api';
+//import { apiSuccess } from '../api/success.api';
 
 const serviceRouter = Router();
 
 serviceRouter.post('/', (req, res) => {
   logger.info('Service post message received');
   logger.info('Request body: ' + JSON.stringify(req.body));
-  apiSuccess(
-    200,
-    [
-      {
-        action: 'setFirstName',
-        firstName: 'Dummy',
-      },
-    ],
-    res
-  );
+  logger.info(res);
 
   /**
   try {
