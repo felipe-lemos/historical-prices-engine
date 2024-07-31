51afdc8285f80492351cc35df2ed245b623d2dd5
Felipe Lemos <>
2024-07-31 10:22:28 +0200

    Test: Return API Success

--- historical-prices/src/routes/service.route.ts
+++ historical-prices/src/routes/service.route.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { logger } from '../utils/logger.utils';
 import { post } from '../controllers/service.controller';
+import { apiSuccess } from '../api/success.api';
 
 const serviceRouter = Router();
 
 serviceRouter.post('/', (req, res) => {
   logger.info('Service post message received');
   logger.info('Request body: ' + JSON.stringify(req.body));
-   res.status(200).end();
+  apiSuccess(200, ['Update'], res);
+
    /**
   try {
     post(req, res);
